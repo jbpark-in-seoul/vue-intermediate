bc54fa1f815e137b27f579c7462996b6f18f770f
jbpark-in-seoul <jbparkinseoul@gmail.com>
2022-12-26 23:44:04 +0900

    feat(component): ToDo 애플리케이션 컴포넌트 추가

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" :key="todoItem">
+        {{ todoItem }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  created: function() { // 인스턴스 생성 직후의 hook
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  },
+  mounted: function() {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
