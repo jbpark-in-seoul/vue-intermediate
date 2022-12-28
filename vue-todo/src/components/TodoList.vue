3db536bac5340853068329e5f5beb84c5ab08303
jbpark-in-seoul <jbparkinseoul@gmail.com>
2022-12-28 23:11:12 +0900

    feat(component): ToDo 애플리케이션 컴포넌트 추가

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" :key="todoItem">
-        {{ todoItem }}
+      <li v-for="(todoItem, index) in todoItems" :key="todoItem.item" class="shadow">
+        <i class="checkBtn fas fa-check" :class="{checkBtnCompleted: todoItem.completed}" @click="toggleComplete(todoItem, index)"></i>
+        <span :class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+        <span class="removeBtn" @click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
       </li>
     </ul>
   </div>
@@ -15,11 +19,22 @@ export default {
       todoItems: []
     }
   },
+  methods: {
+    removeTodo: function(item, index) {
+      localStorage.removeItem(item);
+      this.todoItems.splice(index, 1); // 새로운 배열 반환
+    },
+    toggleComplete: function(item, index) {
+      item.completed = !item.completed;
+      localStorage.removeItem(item.item);
+      localStorage.setItem(item.item, JSON.stringify(item));
+    }
+  }, 
   created: function() { // 인스턴스 생성 직후의 hook
     if (localStorage.length > 0) {
       for (var i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(localStorage.key(i));
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
         }
       }
     }
@@ -31,5 +46,30 @@ export default {
 </script>
 
 <style>
-
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .checkBtnCompleted {
+    color: #b3adad;
+  }
+  .textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
 </style>
